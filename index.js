bc6ea21a001eaad9a186b9f9c59de46163378e63
gabriel <vieyra.gabriel08@gmail.com>
2021-01-11 16:21:35 -0300

    obteniendo de la API información de un pokemon en particular y mostrar algunos datos en un modal

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function mostrarListadoPokemones(pokemones) {
                     >
                         <div class="d-flex flex-column align-items-center">
                             <h4 class="mb-3 text-capitalize">${pokemon.name}</h4>
-                            <button class="btn btn-danger shadow-none" data-id="${pokemon.url}">
+                            <button class="btn btn-danger shadow-none detalle-pokemon" data-bs-toggle="modal" data-bs-target="#modal-detalle-pokemon" data-url="${pokemon.url}">
                                 Ver detalle
                             </button>
                         </div>
@@ -32,6 +32,52 @@ function mostrarListadoPokemones(pokemones) {
                 </article>
         `;
     });
+
+    document.querySelectorAll(".detalle-pokemon").forEach((btn) => {
+        btn.addEventListener("click", mostrarModalDetalle);
+    });
+}
+
+async function mostrarModalDetalle(e) {
+    const url = e.currentTarget.getAttribute("data-url");
+    const img = document.querySelector("#img");
+
+    let pokemon = await cargarPokemon(url);
+    console.log(pokemon);
+
+    img.setAttribute("src", `${pokemon.sprites.front_default}`);
+    img.setAttribute("alt", `Imagen frontal del pokemon ${pokemon.name}`);
+    document.querySelector("#nombre").innerHTML = `${pokemon.name}`;
+    document.querySelector(
+        "#valor-hp"
+    ).innerHTML = `${pokemon.stats[0].base_stat}`;
+    document.querySelector(
+        "#valor-ataque"
+    ).innerHTML = `${pokemon.stats[1].base_stat}`;
+    document.querySelector(
+        "#valor-defensa"
+    ).innerHTML = `${pokemon.stats[2].base_stat}`;
+    document.querySelector(
+        "#valor-velocidad"
+    ).innerHTML = `${pokemon.stats[5].base_stat}`;
+    document.querySelector(
+        "#valor-ataque-especial"
+    ).innerHTML = `${pokemon.stats[3].base_stat}`;
+    document.querySelector(
+        "#valor-defensa-especial"
+    ).innerHTML = `${pokemon.stats[4].base_stat}`;
+}
+
+async function cargarPokemon(pokemon) {
+    let response = await fetch(pokemon);
+
+    if (response.status === 200) {
+        let pokemon = await response.json();
+
+        return pokemon;
+    } else {
+        alert("Algo salio mal");
+    }
 }
 
 iniciar();
